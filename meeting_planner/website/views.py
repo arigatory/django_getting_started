b1fdd8d4ed6085a8a4cbb61d658cb545f65acbf0
Ivan <ivan@ivan.ru>
2021-09-15 19:50:50 +0300

    add detail

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -4,10 +4,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from meetings.models import Meeting
 
 
 def welcome(request):
-    return HttpResponse('Welcome to the Meeting Planner!')
+    return render(request, "website/welcome.html", {"num_meetings": Meeting.objects.count()})
 
 
 def date(request):
